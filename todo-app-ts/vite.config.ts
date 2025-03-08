8769ebb10b6c74756358b574123781f16133029c
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-03-08 16:20:39 +0900

    20250308 Add devcert

--- todo-app-ts/vite.config.ts
+++ todo-app-ts/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), mkcert()],
   server: {
-    port: 3000,
+    https: true,
   },
   build: {
     target: 'esnext',
