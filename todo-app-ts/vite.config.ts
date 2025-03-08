52e27058cdd313238086147ff232c6549c86cda7
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-03-08 16:25:27 +0900

    20250308 Fix server.https

--- todo-app-ts/vite.config.ts
+++ todo-app-ts/vite.config.ts
@@ -4,9 +4,6 @@ import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
   plugins: [solidPlugin(), mkcert()],
-  server: {
-    https: true,
-  },
   build: {
     target: 'esnext',
   },
